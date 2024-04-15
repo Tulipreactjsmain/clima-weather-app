03bdf4f83815a88d38e222cdcda9274122e8eba5
Tulipreactjsmain <rdhnft@gmail.com>
2024-04-15 20:48:31 +0100

    clima-APP setup HTTP

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../services/location.dart';
+import 'package:http/http.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Location location = Location();
+   await location.getCurrentLocation();
+   print(location.latitude);
+   print(location.longitude);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
